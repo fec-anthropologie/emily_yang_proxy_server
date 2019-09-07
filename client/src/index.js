fe05df5cb36e15aa49532149fd9eebc706ab92a9
Emily Yang <emilyyang@Emilys-MacBook-Air.local>
2019-09-06 20:04:30 -0700

    added client side

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(<Search />, document.getElementById('search'));
+// ReactDOM.render(<Review />, document.getElementById('review'));
+// ReactDOM.render(<Product />, document.getElementById('product'));
